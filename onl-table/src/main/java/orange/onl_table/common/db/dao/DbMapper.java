9fc267cc536cb17f2361ce74696d72e8312b30c9
orange <280790081@qq.com>
2019-11-30 14:51:48 +0800

    更新onl_table

--- onl-table/src/main/java/orange/onl_table/common/db/dao/DbMapper.java
+++ onl-table/src/main/java/orange/onl_table/common/db/dao/DbMapper.java
@@ -4,6 +4,7 @@ import orange.onl_table.common.db.entity.DbColumn;
 import orange.onl_table.common.db.entity.DbTable;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 import java.util.Map;
@@ -11,14 +12,19 @@ import java.util.Map;
 @Mapper
 public interface DbMapper {
 
+    @Select("${sql}")
     Object selectObject(@Param("sql") String sql);
 
+    @Select("${sql}")
     DbTable tableOne(@Param("sql") String sql);
 
+    @Select("${sql}")
     Boolean tableCheckExist(@Param("sql") String sql);
 
+    @Select("${sql}")
     List<DbTable> tableList(@Param("sql") String sql);
 
+    @Select("${sql}")
     List<DbColumn> columnList(@Param("sql") String sql);
 
 }
--- onl-table/src/main/java/orange/onl_table/dao/OnlDataMapper.java
+++ onl-table/src/main/java/orange/onl_table/dao/OnlDataMapper.java
@@ -2,8 +2,7 @@ package orange.onl_table.dao;
 
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 import java.util.Map;
@@ -11,18 +10,25 @@ import java.util.Map;
 @Mapper
 public interface OnlDataMapper {
 
+    @Select("${sql}")
     Map<String, Object> executeOneSQL(@Param("sql") String sql, @Param("param") Map<String, Object> param);
 
+    @Select("${sql}")
     Long executeCountSQL(@Param("sql") String sql, @Param("param") Map<String, Object> param);
 
+    @Select("${sql}")
     List<Map<String, Object>> executeListSQL(@Param("sql") String sql, @Param("param") Map<String, Object> param);
 
+    @Select("${sql}")
     IPage<Map<String, Object>> executePageSQL(IPage page, @Param("sql") String sql, @Param("param") Map<String, Object> param);
 
+    @Insert("${sql}")
     int executeInsertSQL(@Param("sql") String sql, @Param("param") Map<String, Object> param);
 
+    @Update("${sql}")
     int executeUpdateSQL(@Param("sql") String sql, @Param("param") Map<String, Object> param);
 
+    @Delete("${sql}")
     int executeDeleteSQL(@Param("sql") String sql, @Param("param") Map<String, Object> param);
 
 }
